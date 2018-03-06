db0397b11e8227692767f77a1970cdfe4545ac81
Mattias Lundberg <mattias@kundo.se>
2018-03-06 18:57:18 +0100

    sublist

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -4,5 +4,32 @@ defmodule Sublist do
   and if not whether it is equal or unequal to the second list.
   """
   def compare(a, b) do
+    cond do
+      a === b ->
+        :equal
+
+      length(a) == length(b) and a !== b ->
+        :unequal
+
+      sublist?(b, a) ->
+        :sublist
+
+      sublist?(a, b) ->
+        :superlist
+
+      true ->
+        :unequal
+    end
+  end
+
+  defp sublist?(_, []), do: true
+  defp sublist?([], _), do: false
+
+  defp sublist?(a = [h | r], b) do
+    if h === hd(b) and Enum.take(a, Enum.count(b)) == b do
+      true
+    else
+      sublist?(r, b)
+    end
   end
 end
