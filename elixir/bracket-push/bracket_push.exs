bac1a506367f98c0f0890687d9b7c82d466c0b2a
Mattias Lundberg <mattias@kundo.se>
2018-03-09 19:02:25 +0100

    Submitted bracked push

--- elixir/bracket-push/bracket_push.exs
+++ elixir/bracket-push/bracket_push.exs
@@ -4,5 +4,34 @@ defmodule BracketPush do
   """
   @spec check_brackets(String.t()) :: boolean
   def check_brackets(str) do
+    unmatched =
+      str
+      |> String.split("", trim: true)
+      |> Enum.reduce_while([], &check_bracket/2)
+
+    length(unmatched) == 0
+  end
+
+  defp check_bracket(chr, acc) do
+    cond do
+      chr == "{" ->
+        {:cont, ["}" | acc]}
+
+      chr == "[" ->
+        {:cont, ["]" | acc]}
+
+      chr == "(" ->
+        {:cont, [")" | acc]}
+
+      chr in ["}", "]", ")"] and chr == List.first(acc) ->
+        {_, tail} = List.pop_at(acc, 0)
+        {:cont, tail}
+
+      chr in ["}", "]", ")"] ->
+        {:halt, ["X"]}
+
+      true ->
+        {:cont, acc}
+    end
   end
 end
