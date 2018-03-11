43d26729533d3013e0957413db337c960908cad2
Mattias Lundberg <mattias@kundo.se>
2018-03-11 16:52:07 +0100

    implementation of leap

--- elixir/leap/leap.exs
+++ elixir/leap/leap.exs
@@ -10,5 +10,11 @@ defmodule Year do
   """
   @spec leap_year?(non_neg_integer) :: boolean
   def leap_year?(year) do
+    cond do
+      rem(year, 4) != 0 -> false
+      rem(year, 100) != 0 -> true
+      rem(year, 400) != 0 -> false
+      true -> true
+    end
   end
 end
