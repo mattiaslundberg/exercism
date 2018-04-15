dc9529a0b44e5ae19daf470ce779b8b90ff2fa56
Mattias Lundberg <mattias@kundo.se>
2018-04-15 15:52:16 +0200

    rna

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,15 @@
+TRANSLATIONS = {
+    "C": "G",
+    "G": "C",
+    "T": "A",
+    "A": "U",
+}
+
+
 def to_rna(dna_strand):
-    pass
+    return "".join(list(map(chr_to_rna, dna_strand)))
+
+
+def chr_to_rna(chr):
+
+    return TRANSLATIONS[chr]
